3fd293f1ca8488d2a8d99c0910e3d4923fe63a91
gt <mr.tazhibayev@gmail.com>
2024-04-24 21:23:22 -0400

    Finished own solution for this problem

--- solutions/catr/src/lib.rs
+++ solutions/catr/src/lib.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -8,6 +11,14 @@ pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+}
+
+// Using box to store values in Heap, because the system doesn't know how much memory it might need. So the stack is not suitable here.
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?)))
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -50,9 +61,34 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+fn print(reader: &mut Box<dyn BufRead>, number_lines: bool, number_nonblank_lines: bool) {
+    let mut line_num = 1;
+
+    for line in reader.lines() {
+        let text = line.unwrap();
+
+        if number_lines {
+            print!("     {}\t{}\n", line_num, text);
+            line_num += 1;
+        } else if number_nonblank_lines {
+            if text != "" {
+                print!("     {}\t{}\n", line_num, text);
+                line_num += 1;
+            } else {
+                println!("{}", text)
+            }
+        } else {
+            println!("{}", text);
+        }
+    } 
+}
+
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
-        println!("{}", filename)
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(_) => print(&mut open(&filename).unwrap(), config.number_lines, config.number_nonblank_lines)
+        }
     }
     Ok(())
 }
